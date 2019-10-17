80d315f5feea491afcaac221608c46c2938717e5
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-10-17 09:50:22 -0400

    Work from previous day

---
+++ README.md
@@ -0,0 +1,13 @@
+Starting the process of Learning Ruby.
+
+Day 1: 
+  - Working on Ruby and Ruby on Rails Udemy Course 
+  - Basics like print vs puts (put string), and gets_chomp to store user input into a variable
+Day 2: 
+  - Worked on Branching...(If / elsif statements)
+  - Arrays and array methods 
+  - a.uniq! mutates a and removes duplicate values 
+  - %w(Hello my names Zach) takes the string and turns it into an array ["Hello", "my", "names", "Zach"]
+  - Ranges x = 1..100 is x is a range of 1 to 100. I could then to x.to_a to print x to an array
+  - .each is the prefered way of iterating through the array
+  - 
--- playground.rb
+++ playground.rb
@@ -1,13 +1,44 @@
-puts "What is your first name?"
-first_name = gets.chomp
+# puts "What is your first name?"
+# first_name = gets.chomp
 
-puts "What is your last name?"
-last_name = gets.chomp
+# puts "What is your last name?"
+# last_name = gets.chomp
 
-full_name = first_name + last_name
+# full_name = first_name + last_name
 
-puts "Your full name is #{first_name} #{last_name}"
+# puts "Your full name is #{first_name} #{last_name}"
 
-puts "Your full name reversed is #{last_name.reverse} #{first_name.reverse}"
+# puts "Your full name reversed is #{last_name.reverse} #{first_name.reverse}"
 
-puts "Your name has #{full_name.length} characters in it"
+# puts "Your name has #{full_name.length} characters in it"
+
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+
+puts
+puts "Simple Calculator"
+25.times { print "-" }
+puts 
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+puts "What do you want to do?"
+puts "Enter 1 for multiplication, 2 for addition, 3 for subtraction"
+user_entry = gets.chomp
+
+if user_entry == "1"
+  puts "The first number multiplyed by the second number is: #{ multiply(num_1.to_i, num_2.to_i) }"
+elsif user_entry == "2"
+  puts "The first number added to the second number is: #{num_1.to_i + num_2.to_i}"
+elsif user_entry == "3"
+  # puts "The first number divided by the second number is: #{num_1.to_i / num_2.to_i}"
+  puts "The second number subtracted from the first number is: #{num_1.to_i - num_2.to_i}"
+else
+  puts "Invalid Entry"
+end
+
+
