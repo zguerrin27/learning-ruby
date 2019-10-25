9d853b98cc1207d3818804c78f275dd93a53c1b6
Zachary Guerrin <39317224+zguerrin27@users.noreply.github.com>
2019-10-25 11:39:09 -0400

    Working on blog app

--- rails_projects/alpha-blog/config/routes.rb
+++ rails_projects/alpha-blog/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'welcome/home', to: 'welcome#home'
-  get 'welcome/about', to: 'welcome#about'
-
+  root 'pages#home'
+  get 'about', to: 'pages#about'
 
 end
